36b2603a015872ff56c49cd336b783497989b39e
Brian Finck <brian@brianfinck.com>
2017-10-29 18:38:21 -0700

    refactor

--- Main.elm
+++ Main.elm
@@ -1,91 +1,96 @@
+module Main exposing (..)
 import Html exposing (..)
-import Html.Attributes exposing (..)
 import Http
-import Json.Decode exposing (field)
+import Json.Decode as Decode exposing (field)
+import Html.Events exposing (onClick)
 
 
+-- MAIN
+-- main :  Program Never Model Msg
 main =
   Html.program
     { init = init
-    , view = view
     , update = update
-    , subscriptions = always Sub.none
+    , view = view
+    , subscriptions = subscriptions
     }
 
 
 -- MODEL
 
 
-type alias Model =
-  { emails : List String
+type alias Email =
+  { email : String
   }
 
+type alias Model =
+  List Email
 
+type Msg
+  = LoadEmails
+  | LoadedEmails (Result Http.Error (List Email))
+
+
+
+-- INIT
+
+
+init : (Model, Cmd Msg)
 init =
-  ( Model
-  , getEmailList
-  )
+  ([], loadEmails)
 
+loadEmails : Cmd Msg
+loadEmails =
+  Http.send LoadedEmails
+    (Http.get "https://gist.githubusercontent.com/Brian11f/c429d64efc5e3297282259b7eef39c35/raw/fa30dec8137bc34ba7ec0c08d61a6feaa9bf65f6/Emails" decodeEmailList)
+decodeEmail : Decode.Decoder Email
+decodeEmail =
+    Decode.map Email
+        (field "email" Decode.string)
+decodeEmailList : Decode.Decoder (List Email)
+decodeEmailList =
+    Decode.list decodeEmail
 
 
 -- UPDATE
 
 
-type Msg
-    = GetEmails
-    -- | Res (Result Http.Error String)
-
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    case msg of
-      GetEmails ->
-          (model, getEmailList)
-
-      -- Res (Ok newUrl) ->
-      --     (Model model.emails, Cmd.none)
-      --
-      -- Res (Err _) ->
-      --     (model, Cmd.none)
+  case msg of
+    LoadEmails ->
+      (model, loadEmails)
+    LoadedEmails (Ok emails) ->
+      (emails, Cmd.none)
+    LoadedEmails (Err _) ->
+      (model, Cmd.none)
 
 
 -- VIEW
 
 
+view : Model -> Html Msg
 view model =
-    div [ class "content" ]
-        [ viewHeader
-        , ul [ class "results" ] ( List.map viewEmailResult model.emails )
-        ]
+  let
+    result =
+      if ((List.isEmpty model)) then
+        div [][ text "Currently no  emails." ]
+      else
+        div [] (List.map  emailRow model)
+  in
+    div []
+      [ h1 [] [ text "Emails" ]
+      , result
+      ]
+
+emailRow : Email -> Html Msg
+emailRow email =
+  div [] [ text email.email ]
 
 
-viewHeader =
-    header []
-        [ h1 [] [ text "Email Test" ]
-        , span [ class "tagline" ] [ text "Http test and list filtering in Elm" ]
-        ]
+  -- SUBSCRIPTIONS
 
 
-viewEmailResult email =
-    li []
-        [ p [] [ text email ]
-        ]
-
-
--- HTTP
-
-
-getEmailList =
-    let
-      url =
-        "https://gist.githubusercontent.com/Brian11f/76832748487900aff1bc833719302072/raw/f32c7a440db1e5e38e9dfa732fc47ec8879f8eba/emails"
-    in
-      Http.get url decodeEmailUrl
-
-
-type alias Emails =
-    { emails : List String
-    }
-
-
-decodeEmailUrl =
-    Json.Decode.map Emails
-        (field "emails" Json.Decode.string)
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none

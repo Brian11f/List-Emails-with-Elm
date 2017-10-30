0d7df0b2837507b182ba4645f4a19458fbd2518c
Brian Finck <brian@brianfinck.com>
2017-10-29 19:57:16 -0700

    Filter function nothing is working

--- Main.elm
+++ Main.elm
@@ -2,7 +2,8 @@ module Main exposing (..)
 import Html exposing (..)
 import Http
 import Json.Decode as Decode exposing (field)
-import Html.Events exposing (onClick)
+import List exposing (..)
+import Set exposing (Set)
 
 
 -- MAIN
@@ -41,7 +42,7 @@ init =
 loadEmails : Cmd Msg
 loadEmails =
   Http.send LoadedEmails
-    (Http.get "https://raw.githubusercontent.com/Brian11f/List-Emails-with-Elm/master/ListOfEmails.json" decodeEmailList)
+    (Http.get "https://raw.githubusercontent.com/Brian11f/List-Emails-with-Elm/d092df1debb7805514efcec50e150fc7311941d6/ListOfEmails.json" decodeEmailList)
 decodeEmail : Decode.Decoder Email
 decodeEmail =
     Decode.map Email
@@ -61,8 +62,26 @@ update msg model =
       (model, loadEmails)
     LoadedEmails (Ok emails) ->
       (emails, Cmd.none)
+      (filterEmails model)
     LoadedEmails (Err _) ->
       (model, Cmd.none)
+
+-- Compare Function
+filterEmails : List comparable -> List comparable
+filterEmails list =
+  uniqueHelp Set.empty list
+
+uniqueHelp : Set comparable -> List comparable -> List comparable
+uniqueHelp existing remaining =
+  case remaining of
+    [] ->
+      []
+
+    first :: rest ->
+      if Set.member first existing then
+        uniqueHelp existing rest
+      else
+        first :: uniqueHelp (Set.insert first existing) rest
 
 
 -- VIEW
@@ -75,7 +94,7 @@ view model =
       if ((List.isEmpty model)) then
         div [][ text "Currently no  emails." ]
       else
-        div [] (List.map  emailRow model)
+        div [] (List.map emailRow model)
   in
     div []
       [ h1 [] [ text "Emails" ]
